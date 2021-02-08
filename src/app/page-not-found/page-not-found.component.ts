d6f9be96524c51760b19a13e142f448199820129
Kemar Bell <bellstpatrick@gmail.com>
2021-02-07 21:30:38 -0500

    404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'portfolio-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    const visual = document.getElementById('visual');
+    const events = ['resize', 'load'];
+
+    events.forEach((e) => {
+      window.addEventListener(e, () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const ratio = 45 / (width / height);
+        if (visual) {
+          visual.style.transform =
+            'translate(-50%, -50%) rotate(-' + ratio + 'deg)';
+        }
+      });
+    });
   }
 
+  goHome(): void {
+    this.router.navigateByUrl('/');
+  }
 }
