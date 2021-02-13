421866b6971d42f634def7cad5bb92f698d94c8d
Kemar Bell <bellstpatrick@gmail.com>
2021-02-13 15:51:44 -0500

    data + cleanup + style fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'portfolio-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router) {}
 
+  public get isContactRoute(): boolean {
+    return this.router.url === '/contact';
+  }
 }
