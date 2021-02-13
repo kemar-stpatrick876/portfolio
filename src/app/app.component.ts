7d1fce59ca6254a518e7d3f65fc4fa1fe28b311f
Kemar Bell <bellstpatrick@gmail.com>
2021-02-13 18:56:27 -0500

    missing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'portfolio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private router: Router) {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.el.nativeElement.scrollTo(0, 0);
+      });
+  }
 
   public get isContactRoute(): boolean {
     return this.router.url === '/contact';
